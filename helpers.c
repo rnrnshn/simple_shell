1e2180cb128ed2fbd0b546faa0c22927e1e1817e
rnrnshn <olymangue@gmail.com>
2022-11-14 10:16:17 +0200

    free the cmd array

--- helpers.c
+++ helpers.c
@@ -81,3 +81,24 @@ char **tokenize(char *str)
 	free(buf);
 	return (tokens);
 }
+
+/**
+ * free_cmds - This program frees the cmd array
+ * @m: pointer to an array of command line
+ * arguments
+ *
+ * Return: void
+ */
+void free_cmds(char **m)
+{
+	int index = 0;
+
+	if (m == NULL)
+		return;
+	while (m[index])
+	{
+		free(m[index]);
+		index++;
+	}
+	free(m);
+}
