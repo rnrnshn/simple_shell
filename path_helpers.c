533fb2aee857ee57c83552dccd7efe4495edc6f6
rnrnshn <olymangue@gmail.com>
2022-11-14 10:23:25 +0200

    search path dir

--- path_helpers.c
+++ path_helpers.c
@@ -29,3 +29,32 @@ char *_getpath(void)
 	}
 	return (NULL);
 }
+
+/**
+ * search_path - searches for PATH dir
+ * containing command
+ * @p: first operand
+ * @cmd: second operand
+ *
+ * Return: unsigned int character count
+ */
+
+char *search_path(char **p, char *cmd)
+{
+	int i = 0;
+
+	char *ret;
+
+	while (p[i])
+	{
+		ret = append_path(p[i], cmd);
+		if (access(ret, F_OK | X_OK) == 0)
+		{
+			return (ret);
+		}
+		else
+			free(ret);
+		i++;
+	}
+	return (NULL);
+}
