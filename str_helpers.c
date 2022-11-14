3ef543501091f3355abeca8dccea4932aef773ee
rnrnshn <olymangue@gmail.com>
2022-11-14 11:57:29 +0200

    duplicate string

--- str_helpers.c
+++ str_helpers.c
@@ -27,3 +27,39 @@ int _strcmp(char *s1, char *s2)
 	return (tot);
 
 }
+
+/**
+ * duplicate_str - returns a pointer to a newly allocated
+ * space in memory, which contains a copy of the
+ * string given as a parameter
+ * @str: string to be duplicated
+ *
+ * Return: pointer to the duplicated string
+ * or NULL if it fails
+ */
+
+char *duplicate_str(char *str)
+{
+	char *p;
+	int i, len;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	len = _strlen(str);
+
+	p = malloc(sizeof(char) * (len + 1));
+	if (p == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; *str != '\0'; str++, i++)
+	{
+		p[i] = str[0];
+	}
+	p[i++] = '\0';
+
+	return (p);
+}
