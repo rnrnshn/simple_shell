1f7542f463daf4bdd00836a8b9ec57875d326d87
rnrnshn <olymangue@gmail.com>
2022-11-14 12:03:18 +0200

    look for a specific char in string

--- str_helpers.c
+++ str_helpers.c
@@ -101,3 +101,22 @@ int _strlen(const char *str)
 		i++;
 	return (i);
 }
+
+/**
+ * check_str - searches a string for a specific character
+ * @str: string to examine
+ * @chr: character to look for
+ *
+ * Return: pointer to index of first occurence, NULL if not found
+ */
+
+char *check_str(char *str, int chr)
+{
+	while (*str)
+	{
+		if (*str == chr)
+			return (str);
+		str++;
+	}
+	return (NULL);
+}
